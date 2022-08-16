344d98bb7b7417fb90d8a3be6261f9a80fa6063f
Adhemar <adhemarduran@gmail.com>
2022-08-16 17:21:42 -0400

    modify php mailer account

--- AppSalon_PHP_MVC_JS_SASS/classes/Email.php
+++ AppSalon_PHP_MVC_JS_SASS/classes/Email.php
@@ -22,8 +22,8 @@ class Email {
         $mail->Host = 'smtp.mailtrap.io';
         $mail->SMTPAuth = true;
         $mail->Port = 2525;
-        $mail->Username = '047a2512e7f17e';
-        $mail->Password = 'e2d3a22ee74122';
+        $mail->Username = '3a5978b90eb666';
+        $mail->Password = '1163ef3d896ca4';
 
         $mail->setFrom('cuentas@appsalon.com');
         $mail->addAddress('cuentas@appsalon.com', 'appsalon.com');
@@ -34,7 +34,7 @@ class Email {
 
         $contenido = "<html>";
         $contenido .= "<p>Hola <strong>". $this->nombre . "</strong> has creado tu cuenta en AppSalon, solo debes confirmarla presionando en el siguiente enlace:</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:8000/confirmar-cuenta?token=" . $this->token . "'>Confirmar cuenta</a></p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confirmar cuenta</a></p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje.</p>";
         $contenido .= "</html>";
 
@@ -50,8 +50,8 @@ class Email {
         $mail->Host = 'smtp.mailtrap.io';
         $mail->SMTPAuth = true;
         $mail->Port = 2525;
-        $mail->Username = '047a2512e7f17e';
-        $mail->Password = 'e2d3a22ee74122';
+        $mail->Username = '3a5978b90eb666';
+        $mail->Password = '1163ef3d896ca4';
 
         $mail->setFrom('cuentas@appsalon.com');
         $mail->addAddress('cuentas@appsalon.com', 'appsalon.com');
@@ -62,7 +62,7 @@ class Email {
 
         $contenido = "<html>";
         $contenido .= "<p>Hola <strong>". $this->nombre . "</strong> has solicitado reestablecer tu password, sigue el siguiente enlace para hacerlo:</p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://localhost:8000/recuperar?token=" . $this->token . "'>Reestablecer password</a></p>";
+        $contenido .= "<p>Presiona aquí: <a href='http://localhost:3000/recuperar?token=" . $this->token . "'>Reestablecer password</a></p>";
         $contenido .= "<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje.</p>";
         $contenido .= "</html>";
 
